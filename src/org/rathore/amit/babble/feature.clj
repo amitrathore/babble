bef553d037d04f3e8a4a8ae50cb3790fbf60134c
Amit Rathore <amitrathore@gmail.com>
2010-01-13 15:19:31 -0800

    added a couple of convenience methods to feature

--- src/org/rathore/amit/babble/feature.clj
+++ src/org/rathore/amit/babble/feature.clj
@@ -4,3 +4,11 @@
   (let [type-spec? #(= attrib-name (first %))
 	extractor (comp next first)]
     (extractor (filter type-spec? specs))))
+
+(defn config-from [specs & attribs]
+  (let [config-kv (fn [attrib]
+                    {(keyword attrib) (first (specs-for attrib specs))})]
+    (apply merge (map config-kv attribs))))
+
+(defmacro nested-config-from [spec-key specs & attribs]
+  `(config-from (specs-for ~spec-key ~specs) ~@attribs))
